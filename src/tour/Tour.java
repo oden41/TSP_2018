58c18558f9103dda625705cbc1c025968988c3a0
shumpmita <shumpmita@ic.dis.titech.ac.jp>
2018-06-14 14:55:52 +0900

    init

---
+++ src/tour/Tour.java
@@ -0,0 +1,11 @@
+package tour;
+
+public class Tour {
+	int[] cityArray;
+	int[] cityIndexArray;
+
+	public Tour(int cityNum) {
+		cityArray = new int[cityNum];
+		cityIndexArray = new int[cityNum];
+	}
+}
